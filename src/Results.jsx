6f94e15b8a0b36f2f388bad650bad10f77ebf886
Tomáš Světnický <tomas.svetnicky@siemens.com>
2021-06-04 09:58:19 +0200

    iframe title

--- src/Results.jsx
+++ src/Results.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Results = () => (
     <iframe
+        title="results"
         src="https://docs.google.com/spreadsheets/d/1VQsiO-AfRgty3qgK1QDsbcpU2pS0uVZfETxZNAkBZhc/edit?usp=sharing"
         style={{ height: '100vh', margin: 0, padding: 0, width: '100vw' }}
     />
